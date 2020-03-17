51166f43a0675c29673e7a623fbd88f110cfc00c
Blake Cooley <45179385+TehBakey@users.noreply.github.com>
2020-03-17 16:09:50 -0500

    Added addLot, deleteAllUserVehicles, module.exports

--- server/db/index.js
+++ server/db/index.js
@@ -17,8 +17,10 @@ pool.connect()
 .then(() => console.log("Connected to DB"))
 .catch(err => console.log(err))
 
-
+//
 //USER
+//
+
 const addUser = (name, email, google_token, image_url, bio, billing_info) => {
     const query = 'INSERT INTO "user" (name, email, google_token, image_url, bio, billing_info) VALUES ($1, $2, $3, $4, $5, $6)'
     pool.query(query, [name, email, google_token, image_url, bio, billing_info]
@@ -47,7 +49,13 @@ const selectUser = (email) => {
 
 // selectUser("test@test.com");
 
+const patchUser = (id) => {
+
+}
+
+//
 //VEHICLE
+//
 
 const addVehicle = (user_id, make, model, color, plate, state) => {
     const query = "INSERT INTO vehicle (user_id, make, model,license_plate, color, state) VALUES ($1, $2, $3, $4, $5, $6)"
@@ -56,7 +64,7 @@ const addVehicle = (user_id, make, model, color, plate, state) => {
     .catch( err => console.log(err))
 }
 
-// addVehicle(3, "Chevy", "Malibu", "12345678", "Silver", "LA" )
+// addVehicle(3, "Ford", "Focus", "12345678", "Silver", "LA" )
 
 const selectVehicle = (id) => {
     const query = "SELECT * FROM vehicle WHERE id = $1"
@@ -68,28 +76,65 @@ const selectVehicle = (id) => {
 // selectVehicle(2)
 
 const deleteVehicle = (id) => {
-
+    const query = 'DELETE FROM vehicle WHERE id = $1'
+    pool.query(query, [id])
+    .then(()=> console.log("Vehicle deleted"))
+    .catch(err => console.log(err))
 }
+
+// deleteVehicle(1)
 
 const deleteAllUserVehicles = (user_id) => {
-
+    const query = 'DELETE FROM vehicle WHERE user_id = $1'
+    pool.query(query, [user_id])
+    .then(()=> console.log("All user vehicles deleted"))
+    .catch(err => console.log(err))
 }
+
+// deleteAllUserVehicles(3)
 
 const patchVehicle = (id, patchArr) => {
 
 }
 
+//
+//LOT
+//
+
+const addLot = (owner_id, image_url, price, longitude, latitude, is_open, lot_close, max_reserve, max_spots, current_spots, description) => {
+    const query = "INSERT INTO lot (owner_id, image_url, price, longitude, latitude, is_open, lot_close, max_reserve, max_spots, current_spots, description) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)"
+    pool.query(query, [owner_id, image_url, price, longitude, latitude, is_open, lot_close, max_reserve, max_spots, current_spots, description])
+    .then(console.log("Lot added!"))
+    .catch( err => console.log(err))
+}
+
+addLot(3, "test_url", 20, 0, 0, true, new Date, new Date, 4, 0, "A test lot")
+
+const selectLot = (id) => {
+    
+}
+
+const deleteLot = (id) => {
+    
+}
+
+const patchLot = (id) => {
+    
+}
+
+//
 //SPOT
+//
 
-const addSpot = () => {
-
-}
-
-const selectSpot = () => {
+const addSpot = (lot_id, user_id) => {
 
 }
 
-const deleteSpot = () => {
+const selectSpot = (id) => {
+
+}
+
+const deleteSpot = (id) => {
 
 }
 
@@ -97,28 +142,16 @@ const patchSpot = () => {
 
 }
 
-//LOT
-
-const addLot = () => {
+const reserveSpot = (lot_id) => {
 
 }
 
-const selectLot = (id) => {
-
-}
-
-const deleteLot = (id) => {
-
-}
-
-const patchLot = (id) => {
-
-}
-
+//
 //REVIEW
+//
 
 const addReview = (id) => {
-
+    
 }
 
 const selectReview = (id) => {
@@ -146,6 +179,7 @@ addSpot,
 selectSpot,
 deleteSpot,
 patchSpot,
+reserveSpot,
 //LOT
 addLot,
 selectLot,
