02bdd6f19e2c4fa20f26d23ca449fda3d6ee7e79
Blake Cooley <45179385+TehBakey@users.noreply.github.com>
2020-03-17 12:18:40 -0500

    Fix select query log, add template for queries

--- server/db/index.js
+++ server/db/index.js
@@ -1,8 +1,10 @@
 const { Client, Pool } = require('pg');
 var fs = require('fs');
 
-var sql = fs.readFileSync('spotlot.sql').toString();
+// for reading schema file but can't figure out how to parse correctly
+// var sql = fs.readFileSync('spotlot.sql').toString();
 
+// Creates new pool
 const pool = new Pool({
     user: "postgres",
     password: "bacool7769",
@@ -10,11 +12,10 @@ const pool = new Pool({
     database: "spotlot"
 })
 
-
+// Connects pool to DB
 pool.connect()
 .then(() => console.log("Connected to DB"))
 .catch(err => console.log(err))
-// .finally(() => client.end());
 
 
 //USER
@@ -35,10 +36,80 @@ const deleteUser = (id) => {
 const selectUser = (email) => {
     const query = 'SELECT * FROM "user" WHERE email = $1'
     pool.query(query, [email])
-    .then( res => console.log(res.fields))
+    .then( res => console.log(res.rows[0]))
     .catch( err => console.log(err))
 }
 
-selectUser("test@test.com");
+// selectUser("test@test.com");
 
+//VEHICLE
 
+const addVehicle = (user_id, make, model, color, plate, state) => {
+    const query = "INSERT INTO vehicle (user_id, make, model,license_plate, color, state) VALUES ($1, $2, $3, $4, $5, $6)"
+    pool.query(query, [user_id, make, model, color, plate, state])
+    .then(console.log("Vehicle added!"))
+    .catch( err => console.log(err))
+}
+
+// addVehicle(3, "Chevy", "Malibu", "12345678", "Silver", "LA" )
+
+const selectVehicle = (id) => {
+    const query = "SELECT * FROM vehicle WHERE id = $1"
+    pool.query(query, [id])
+    .then(res => console.log(res.row[0]))
+    .catch( err => console.log(err))
+}
+
+const deleteVehicle = () => {
+
+}
+
+const patchVehicle = () => {
+
+}
+
+//SPOT
+
+const addSpot = () => {
+
+}
+
+const selectSpot = () => {
+
+}
+
+const deleteSpot = () => {
+
+}
+
+const patchSpot = () => {
+
+}
+
+//LOT
+
+const addLot = () => {
+
+}
+
+const selectLot = (id) => {
+
+}
+
+const deleteLot = (id) => {
+
+}
+
+const patchLot = (id) => {
+
+}
+
+//REVIEW
+
+const addReview = (id) => {
+
+}
+
+const selectReview = (id) => {
+
+}
