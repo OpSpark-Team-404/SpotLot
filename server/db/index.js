f38d7eca05bdda09a2927c602e75f67bd6a8724d
Blake Cooley <45179385+TehBakey@users.noreply.github.com>
2020-03-16 23:00:29 -0500

    Adds addUser query w/ commented out test

--- server/db/index.js
+++ server/db/index.js
@@ -1,9 +1,9 @@
-const { Client } = require('pg');
+const { Client, Pool } = require('pg');
 var fs = require('fs');
 
 var sql = fs.readFileSync('spotlot.sql').toString();
 
-const client = new Client({
+const pool = new Pool({
     user: "postgres",
     password: "bacool7769",
     host: "localhost",
@@ -11,8 +11,17 @@ const client = new Client({
 })
 
 
-client.connect()
+pool.connect()
 .then(() => console.log("Connected to DB"))
 .catch(err => console.log(err))
 // .finally(() => client.end());
 
+const addUser = (image_url, bio, name, billing_info) => {
+    const query = 'INSERT INTO "user" (image_url, bio, name, billing_info) VALUES ($1, $2, $3, $4)'
+    pool.query(query, [image_url, bio, name, billing_info]
+    )
+    .then(() => console.log("User added"))
+    .catch(err => console.log(err))
+}
+
+addUser("test", "test", "test", "test");
