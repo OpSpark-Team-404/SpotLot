d6f44c02ff2c30d8c59de5058f06eca7d75278ba
William Steck <steckwilliam@gmail.com>
2020-03-18 16:05:45 -0500

    LoginPage with google auth

--- client/LoginPage.js
+++ client/LoginPage.js
@@ -1,13 +1,49 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import * as Google from 'expo-google-app-auth';
+import { key } from '../supersecret'
 
-export default function LoginPage(props){
+export default function LoginPage({ navigation }){
+  const [name, onChangeName] = React.useState('');
+  const [photoUrl, onChangePhotoUrl] = React.useState('');
+
+  signIn = async(char) => {
+    try {
+      const result = await Google.logInAsync({
+        androidClientId: key,
+        scopes: ['profile', 'email'],
+      });
+  
+      if (result.type === 'success') {
+        onChangeName(result.user.name);
+        onChangePhotoUrl(result.user.photoUrl);
+        if(char === 'A'){
+          navigation.navigate('MapContainer')
+        }
+        if(char === 'B'){
+          navigation.navigate('SignUpProfile')
+        }
+      } else {
+        console.log('cancelled');
+      }
+    } catch (e) {
+      console.log('error', e);
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Image source={require('../images/spotlot3.png')} style={styles.image} />
       <Text style={styles.text}>SpotLot</Text>
       <Text style={styles.subtext}>find a <Text style={styles.spotlot}>spot</Text>, rent a <Text style={styles.spotlot}>lot</Text></Text>
-      <Button title="Sign in with Google" onPress={() => props.signIn()} />
+      <View style={styles.button}>
+        <Text style={styles.buttonText}>Have an account?</Text>
+        <Button style={styles.button} title="Sign In with Google" onPress={() => signIn('A')} />
+      </View>
+      <View style={styles.button}>
+        <Text style={styles.buttonText}>Need an account?</Text>
+        <Button title="Sign Up with Google" onPress={() => signIn('B')} />
+      </View>
     </View>
   )
 }
@@ -38,5 +74,14 @@ const styles = StyleSheet.create({
   spotlot: {
     color: '#726D9B',
     fontWeight: 'bold'
+  },
+  button: {
+    width: 200,
+    padding: 10
+  },
+  buttonText: {
+    textAlign: 'center',
+    color: '#E5EBEA',
+    fontStyle: 'italic'
   }
 });
