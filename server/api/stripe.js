f66a79448d76c56cf448e0068735e475d35ec321
Blake Cooley <45179385+TehBakey@users.noreply.github.com>
2020-03-18 22:02:20 -0500

    Added react native stripe element depency, started on stripe router

--- server/api/stripe.js
+++ server/api/stripe.js
@@ -1,11 +1,68 @@
-const stripe = require('stripe')('sk_test_...');
+const stripe = require('stripe')('sk_test_RBETXzuPgut6Ka5O91t2SF30002G14KCKS');
 
-module.exports = async function(fastify) {
+module.exports = async function (fastify) {
 
     // All APIs are under route here
-    fastify.get("/create", (req, res) => {
-      console.log("Run a stripe function");
-      res.send("return createStripeUser data");
+
+    //create new stripe user
+    fastify.post("/create/:email", (req, res) => {
+        stripe.customers.create({
+            email: req.params.email,
+        })
+            .then(customer => res.send(customer.id))
+            .catch(error => console.error(error));
     });
 
-  };
+    //get stripe customer object
+    fastify.get("/:id", (req, res) => {
+        stripe.customers.retrieve(
+            req.params.id)
+            .then(customer => res.send(customer))
+            .catch(error => console.error(error));
+    });
+
+    //add bank account to customer
+    fastify.post("/sources/:id", (req, res) => {
+
+        const params = {
+            // mandatory
+            object: "bank_account",
+            country: "US",
+            currency: "usd",
+            account_number: '000123456789',
+            // optional
+            routing_number: '110000000', // 9 digits
+            account_holder_name: 'Test holder name',
+            account_holder_type: 'individual', // "company" or "individual"
+        }
+
+
+
+        stripe.customers.createSource(
+            req.params.id,
+            { source: params })
+            .then( bank_account => res.send(bank_account))
+            .catch(error => console.error(error));
+    });
+
+    //add debit card to customer
+    fastify.post("/payment_methods/:id", (req, res) => {
+        stripe.paymentMethods.create(
+            {
+              type: 'card',
+              card: {
+                number: '4242424242424242',
+                exp_month: 3,
+                exp_year: 2021,
+                cvc: '314',
+              },
+            })
+            .then(reply => {
+                stripe.paymentMethods.attach(
+                    reply.id,
+                    {customer: req.params.id})
+            })
+            .then(reply => res.send(reply))
+            .catch(error => console.error(error));
+        });
+};
