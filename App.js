42dc3c9e50d1ec26c50b1fb23e701b674fbfca95
NaseerHines <Hinesnaseer@gmail.com>
2020-03-21 14:37:44 -0500

    updated file version

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import { Image, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList, DrawerItem } from '@react-navigation/drawer';
-import LoginPage from './client/LoginPage';
-import SignUpProfile from './client/SignUpProfile';
-import SignUpCar from './client/SignUpCar';
-import Profile from './client/Profile';
-import MapContainer from './client/MapContainer';
-import { FontAwesome5 } from '@expo/vector-icons';
+import React from "react";
+import { Image, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem
+} from "@react-navigation/drawer";
+import LoginPage from "./client/LoginPage";
+import SignUpProfile from "./client/SignUpProfile";
+import SignUpCar from "./client/SignUpCar";
+import Profile from "./client/Profile";
+import MyLot from "./client/MyLot";
+import CreateLot from "./client/CreateLot";
+import MapContainer from "./client/MapContainer";
+import { FontAwesome5 } from "@expo/vector-icons";
 
 const LoginStack = createStackNavigator();
 const LoginStackScreen = () => (
@@ -48,51 +55,107 @@ const MapContainerStackScreen = () => (
   </MapContainerStack.Navigator>
 );
 
-function CustomDrawer(props){
+const MyLotStack = createStackNavigator();
+const MyLotStackScreen = () => (
+  <MyLotStack.Navigator
+    screenOptions={{
+      headerShown: false
+    }}
+    initialRouteName="MyLot"
+  >
+    <MyLotStack.Screen name="MyLot" component={MyLot} />
+    <MyLotStack.Screen name="CreateLot" component={CreateLot} />
+  </MyLotStack.Navigator>
+);
+
+function CustomDrawer(props) {
   return (
     <DrawerContentScrollView {...props}>
       <DrawerItem
-        label={() => 
-          <View style={{ flex: 1, flexDirection: 'row' }}>
-            <Image style={{height: 65, width: 65, resizeMode: 'contain'}} source={{ uri: 'https://pngimg.com/uploads/face/face_PNG5645.png' }} />
-            <Text style={{color: '#E5EBEA', alignSelf: "flex-end", fontSize: 16}}>Fresh Prince</Text>
+        label={() => (
+          <View style={{ flex: 1, flexDirection: "row" }}>
+            <Image
+              style={{ height: 65, width: 65, resizeMode: "contain" }}
+              source={{
+                uri: "https://pngimg.com/uploads/face/face_PNG5645.png"
+              }}
+            />
+            <Text
+              style={{ color: "#E5EBEA", alignSelf: "flex-end", fontSize: 16 }}
+            >
+              Fresh Prince
+            </Text>
           </View>
-        }
-        style={{backgroundColor: '#3fb984'}}
+        )}
+        style={{ backgroundColor: "#3fb984" }}
       />
       <DrawerItemList {...props} />
     </DrawerContentScrollView>
-  )
+  );
 }
 
 const Drawer = createDrawerNavigator();
 
-export default function App(){
+export default function App() {
   return (
     <NavigationContainer>
       <Drawer.Navigator
-      initialRouteName="Logout"
-      drawerContentOptions={{
-        activeTintColor: '#3fb984'
-      }}
-      drawerContent={props => <CustomDrawer {...props} />}
+        initialRouteName="Logout"
+        drawerContentOptions={{
+          activeTintColor: "#3fb984"
+        }}
+        drawerContent={props => <CustomDrawer {...props} />}
       >
         <Drawer.Screen
           name="Map"
           component={MapContainerStackScreen}
-          options={{ drawerIcon: ({ tintColor }) => (<FontAwesome5 name="globe" color={tintColor} size={20} right={-10} />) }}
+          options={{
+            drawerIcon: ({ tintColor }) => (
+              <FontAwesome5
+                name="globe"
+                color={tintColor}
+                size={20}
+                right={-10}
+              />
+            )
+          }}
         />
         <Drawer.Screen
           name="Profile"
           component={ProfileStackScreen}
-          options={{ drawerIcon: ({ tintColor }) => (<FontAwesome5 name="user" color={tintColor} size={20} right={-10} />) }}
+          options={{
+            drawerIcon: ({ tintColor }) => (
+              <FontAwesome5
+                name="user"
+                color={tintColor}
+                size={20}
+                right={-10}
+              />
+            )
+          }}
+        />
+        <Drawer.Screen
+          name="MyLot"
+          component={MyLotStackScreen}
+          // options={{ drawerIcon: ({ tintColor }) => (<FontAwesome5 name="user" color={tintColor} size={20} right={-10} />) }}
+          // need to find an icon for MyLot
         />
         <Drawer.Screen
           name="Logout"
           component={LoginStackScreen}
-          options={{ gestureEnabled: false, drawerIcon: ({ tintColor }) => (<FontAwesome5 name="sign-out-alt" color={tintColor} size={20} right={-10} />)}}
+          options={{
+            gestureEnabled: false,
+            drawerIcon: ({ tintColor }) => (
+              <FontAwesome5
+                name="sign-out-alt"
+                color={tintColor}
+                size={20}
+                right={-10}
+              />
+            )
+          }}
         />
       </Drawer.Navigator>
     </NavigationContainer>
-  )
+  );
 }
