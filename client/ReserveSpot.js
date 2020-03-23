14485bbfa207b4c18d3f0654c91f61e6a74caabf
William Steck <steckwilliam@gmail.com>
2020-03-22 19:02:11 -0500

    Made payment page

---
+++ client/ReserveSpot.js
@@ -0,0 +1,112 @@
+import React from 'react';
+import { Text, View, Image, StyleSheet, Button, TouchableOpacity, TextInput } from 'react-native';
+import { FontAwesome5 } from '@expo/vector-icons';
+
+export default function ReserveSpot({ navigation }){
+  const [name, changeName] = React.useState('');
+  const [number, changeNumber] = React.useState('');
+  const [expiration, changeExpiration] = React.useState('');
+  const [cvc, changeCVC] = React.useState('');
+  const [zip, changeZip] = React.useState('');
+
+  function saveToDB(){
+
+  }
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={{ alignSelf: "flex-start", margin: 16, left: -60, top: -180 }}
+        onPress={() => navigation.goBack()}
+        >
+        <FontAwesome5 name="arrow-left" size={24} color='#3fb984' />
+      </TouchableOpacity>
+      <Text style={styles.header}>Credit card information</Text>
+      <View style={{ top: -200}}>
+        <View style={{top: 5}}>
+          <Text style={styles.inputHeader}>Credit card name</Text>
+          <TextInput
+            style={styles.textInput}
+            onChangeText={text => changeName(text)}
+          >
+          </TextInput>
+        </View>
+        <View style={{top: 10}}>
+          <Text style={styles.inputHeader}>Credit card number</Text>
+          <TextInput
+            style={styles.textInput}
+            onChangeText={text => changeNumber(text)}
+          >
+          </TextInput>
+        </View>
+        <View style={{flexDirection: 'row', justifyContent: 'space-between' }}>
+          <View style={{top: 15}}>
+            <Text style={styles.inputHeader}>Exp.</Text>
+            <TextInput
+              style={styles.smallInput}
+              onChangeText={text => changeExpiration(text)}
+            >
+            </TextInput>
+          </View>
+          <View style={{top: 15}}>
+            <Text style={styles.inputHeader}>CVC</Text>
+            <TextInput
+              style={styles.smallInput}
+              onChangeText={text => changeCVC(text)}
+            >
+            </TextInput>
+          </View>
+          <View style={{top: 15}}>
+            <Text style={styles.inputHeader}>Zip</Text>
+            <TextInput
+              style={styles.smallInput}
+              onChangeText={text => changeZip(text)}
+            >
+            </TextInput>
+          </View>
+        </View>
+        <View style={{ top: 60 }}>
+          <Button color="#394648" title="Reserve Spot" onPress={() => saveToDB()} />
+        </View>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#E5EBEA',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 55,
+  },
+  header: {
+    fontSize: 25,
+    color: '#222222',
+    fontWeight: 'bold',
+    alignSelf: 'flex-start',
+    top: -200
+  },
+  inputHeader: {
+    color: '#222222',
+    bottom: 5,
+    top: 10
+  },
+  textInput: {
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: '#3fb984',
+    width: 300,
+    top: 10,
+    backgroundColor: 'white',
+  },
+  smallInput: {
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: '#3fb984',
+    width: 90,
+    top: 10,
+    backgroundColor: 'white',
+  }
+})
