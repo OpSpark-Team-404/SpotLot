b4dfbbc60b5eb6fba962ba71d42b4222825b25fc
Blake Cooley <45179385+TehBakey@users.noreply.github.com>
2020-03-28 12:00:01 -0500

    Update Schema

--- spotlot.sql
+++ spotlot.sql
@@ -1,19 +1,14 @@
 CREATE TABLE "user" (
   "id" SERIAL PRIMARY KEY,
   "image_url" varchar(255),
-  "bio" varchar(255),
   "name" varchar(64),
   "email" text UNIQUE,
-  "google_token" text UNIQUE
+  "google_token" text UNIQUE,
+  "phone" varchar(20),
+  "lot_open" int,
+  "spot_open" int
 );
 
-CREATE TABLE "review" (
-  "id" SERIAL PRIMARY KEY,
-  "user_id" int,
-  "lot_id" int,
-  "rating" int,
-  "description" varchar(255)
-);
 
 CREATE TABLE "spot" (
   "id" SERIAL PRIMARY KEY,
@@ -43,14 +38,13 @@ CREATE TABLE "lot" (
   "max_reserve" integer,
   "max_spots" integer,
   "current_spots" integer,
-  "description" varchar(255)
+  "description" varchar(255),
+  "address" varchar(100)
 );
 
 CREATE TABLE "billing_info" (
   "user_id" integer,
   "stripe_token" text,
-  "card_token" text,
-  "bank_token" text
 );
 
 ALTER TABLE "spot" ADD FOREIGN KEY ("user_id") REFERENCES "user" ("id");
@@ -61,8 +55,16 @@ ALTER TABLE "lot" ADD FOREIGN KEY ("owner_id") REFERENCES "user" ("id");
 
 ALTER TABLE "vehicle" ADD FOREIGN KEY ("user_id") REFERENCES "user" ("id");
 
-ALTER TABLE "review" ADD FOREIGN KEY ("user_id") REFERENCES "user" ("id");
-
-ALTER TABLE "review" ADD FOREIGN KEY ("lot_id") REFERENCES "lot" ("id");
-
 ALTER TABLE "billing_info" ADD FOREIGN KEY ("user_id") REFERENCES "user" ("id");
+
+-- CREATE TABLE "review" (
+--   "id" SERIAL PRIMARY KEY,
+--   "user_id" int,
+--   "lot_id" int,
+--   "rating" int,
+--   "description" varchar(255)
+-- );
+
+-- ALTER TABLE "review" ADD FOREIGN KEY ("user_id") REFERENCES "user" ("id");
+
+-- ALTER TABLE "review" ADD FOREIGN KEY ("lot_id") REFERENCES "lot" ("id");
