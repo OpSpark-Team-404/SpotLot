ec82f82ef48841f0b8228855d01342fdc6801a0b
William Steck <steckwilliam@gmail.com>
2020-03-29 16:16:08 -0500

    fixed issue with map gestures

--- client/MapContainer.js
+++ client/MapContainer.js
@@ -1,12 +1,12 @@
-import React from "react";
-import MapView, { Marker, UrlTile } from "react-native-maps";
-import SearchButton from "./SearchButton";
-import SearchInput from "./SearchInput";
-import { StyleSheet, Image, View, TouchableOpacity, Text } from "react-native";
-import { mapStyle } from "./mapStyle";
-import { FontAwesome5 } from "@expo/vector-icons";
-import axios from "axios";
-import { Input } from "react-native-elements";
+import React from 'react';
+import MapView, { Marker, UrlTile } from 'react-native-maps'
+import SearchButton from './SearchButton';
+import SearchInput from './SearchInput';
+import { StyleSheet, Image, View, TouchableOpacity, Text} from 'react-native';
+import { mapStyle } from './mapStyle';
+import { FontAwesome5 } from '@expo/vector-icons';
+import axios from 'axios';
+
 class MapContainter extends React.Component {
   constructor(props) {
     super(props);
@@ -15,87 +15,77 @@ class MapContainter extends React.Component {
       lat: 29.9511,
       lng: -90.031533,
       markers: [],
-      hasReservedSpot: false,
+      reservedLat: 32.78306,
+      reservedLng: -96.80667,
+      hasReserved: true
     };
-    // this.onSearchButtonClick = this.onSearchButtonClick.bind(this);
+    this.onSearchButtonClick = this.onSearchButtonClick.bind(this);
     this.onBlurInput = this.onBlurInput.bind(this);
     this.changeMapCords = this.changeMapCords.bind(this);
-    this.setMapCords = this.setMapCords.bind(this);
     this.getLots = this.getLots.bind(this);
   }
+
   componentDidMount() {
     this.getLots();
   }
-  setMapCords(lat, lng) {
+
+  changeMapCords(lat, lng){
     this.setState({
       lat: lat,
       lng: lng
-    });
+    })
   }
-  changeMapCords(lat, lng) {
+
+  onSearchButtonClick() {
     this.setState({
-      lat: lat,
-      lng: lng
-    });
+      isSearchButtonClicked: !this.state.isSearchButtonClicked,
+    })
   }
-  setReservedSpot() {
-    this.setState({
-      hasReservedSpot: !this.state.hasReservedSpot
-    });
-  }
+
   onBlurInput() {
     this.setState({
-      isSearchButtonClicked: false
-    });
+      isSearchButtonClicked: false,
+    })
   }
-  getLots() {
-    axios
-      .get("http://10.0.2.2:8080/lot/allLots")
-      .then(res => {
+
+  getLots(){
+    axios.get('http://10.0.2.2:8080/lot/allLots')
+      .then((res) => {
         this.setState({
-          markers: res.data
-        });
+          markers: res.data,
+        })
       })
-      .catch(res => {
-        console.log("error getting markers");
+      .catch((res) => {
+        console.log('error getting markers');
       });
   }
+
   render() {
-    const { markers, lat, lng } = this.state;
-    let placeHolder = <Text></Text>;
-    // let SearchInputHolder = (
-    //   <SearchInput
-    //     changeCords={this.setMapCords}
-    //     onBlurFunc={this.onBlurInput}
-    //   />
-    // );
+    const { markers, lat, lng, reservedLat, reservedLng, hasReserved } = this.state
+
     return (
       <View style={styles.container}>
         <MapView
           showsCompass={false}
-          customMapStyle={mapStyle}
+          customMapStyle={mapStyle} 
           style={styles.map}
           region={{
             latitude: lat,
             longitude: lng,
-            latitudeDelta: 0.5,
-            longitudeDelta: 0.5
+            latitudeDelta: .5,
+            longitudeDelta: .5,
           }}
         >
           {markers.map(marker => (
             <Marker
               key={marker.id}
-              coordinate={{
-                latitude: parseFloat(marker.latitude),
-                longitude: parseFloat(marker.longitude)
-              }}
-              image={
-                "https://cdn.mapmarker.io/api/v1/font-awesome/v5/pin?icon=fa-car&size=120&background=3FB984&color=222222&hoffset=0&voffset=-1"
-              }
-              onPress={() => {
-                this.props.navigation.navigate("LotInfo", {
-                  lot: marker
-                });
+              coordinate={{latitude: parseFloat(marker.latitude), longitude: parseFloat(marker.longitude)}}
+
+              image={'https://cdn.mapmarker.io/api/v1/font-awesome/v5/pin?icon=fa-car&size=120&background=3FB984&color=222222&hoffset=0&voffset=-1'}
+              onPress={()=>{
+                this.props.navigation.navigate('LotInfo', {
+                  lot: marker,
+                })
               }}
             >
               <MapView.Callout tooltip={true} />
@@ -106,44 +96,83 @@ class MapContainter extends React.Component {
           style={{ alignSelf: "flex-start", margin: 16, top: 20 }}
           onPress={() => this.props.navigation.openDrawer()}
         >
-          <FontAwesome5 name="bars" size={30} color="#3fb984" />
+          <FontAwesome5 name="bars" size={30} color='#3fb984' />
         </TouchableOpacity>
-        <Image source={require("../images/logo.png")} style={styles.logo} />
+        <Image source={require('../images/logo.png')} style={styles.logo} />
         <TouchableOpacity
           style={{ alignSelf: "flex-end", margin: 16, top: -90 }}
-          onPress={() => this.props.navigation.navigate("ListView")}
+          onPress={() => this.props.navigation.navigate('ListView')}
         >
-          <FontAwesome5 name="list-ul" size={30} color="#3fb984" />
+          <FontAwesome5 name="list-ul" size={30} color='#3fb984' />
         </TouchableOpacity>
-        <SearchButton reserved={this.state.hasReservedSpot}/>
-        <SearchInput
-          changeCords={this.setMapCords}
-          onBlurFunc={this.onBlurInput}
-        />
+        <Text style={styles.textInput} onPress={this.onSearchButtonClick}>  Search for spots...</Text>
+        {this.state.isSearchButtonClicked ?
+          <SearchInput changeCords={this.changeMapCords} onBlurFunc={this.onBlurInput}/>
+          :
+          null
+        }
+        {hasReserved ?
+          <TouchableOpacity
+              style={{
+                alignSelf: "flex-end",
+                justifyContent: "flex-end",
+                top: 375,
+                right: 10
+              }}
+              onPress={() => this.changeMapCords(reservedLat, reservedLng)}
+            >
+              <FontAwesome5 name="map-marked" size={30} color="#3FB984" />
+            </TouchableOpacity>
+          :
+          <TouchableOpacity
+            style={{
+              alignSelf: "flex-end",
+              justifyContent: "flex-end",
+              top: 375,
+              right: 10
+            }}
+          >
+            <FontAwesome5 name="map-marked" size={30} color="#394648" />
+          </TouchableOpacity>
+        }
       </View>
     );
   }
 }
+
 const styles = StyleSheet.create({
   container: {
     position: "absolute",
     top: 0,
     left: 0,
     bottom: 0,
-    right: 0
+    right: 0,
   },
   map: {
-    position: "absolute",
+    position: 'absolute',
     top: 0,
     left: 0,
     bottom: 0,
-    right: 0
+    right: 0,
   },
   logo: {
     height: 50,
     width: 50,
-    alignSelf: "center",
+    alignSelf: 'center',
     top: -40
-  }
+  },
+  textInput: {
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: '#3fb984',
+    color: '#222222',
+    width: 411,
+    height: 45,
+    top: 460,
+    backgroundColor: 'white',
+    textAlignVertical: 'center',
+    fontSize: 20
+  },
 });
+
 export default MapContainter;
--- client/SearchButton.js
+++ client/SearchButton.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import { FontAwesome5 } from "@expo/vector-icons";
 import {StyleSheet, Text, View, TouchableOpacity, Image} from 'react-native';
 
 export default class Search extends Component{
@@ -12,33 +11,16 @@ export default class Search extends Component{
   render() {
     const { clickFunc } = this.props;
     return (
-      <View>
-        {this.props.reserved ? (
-          <TouchableOpacity
-            style={{
-              alignSelf: "flex-end",
-              justifyContent: "flex-end",
-              top: 460,
-              right: 25
-            }}
-            onPress={() => navigation.openDrawer()}
-          >
-            <FontAwesome5 name="location-arrow" size={30} color="#3fb984" />
-          </TouchableOpacity>
-        ) : (
-          <TouchableOpacity
-            style={{
-              alignSelf: "flex-end",
-              justifyContent: "flex-end",
-              top: 460,
-              right: 25
-            }}
-            onPress={() => navigation.openDrawer()}
-          >
-            <FontAwesome5 name="location-arrow" size={30} color="#395C6B" />
-          </TouchableOpacity>
-        )}
-      </View>
+        <TouchableOpacity 
+          style={styles.button}
+          title={'btn'}
+          onPress={()=>{clickFunc()}}
+        >
+          <Image
+            style={styles.image}
+            source={require('../images/search.png')}
+          />
+        </TouchableOpacity>
     );
   }
 }
